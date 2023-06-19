c701dfeea27328d63aa26b4fb16437e1e5664506
Andrei Shapel <mail@andreishapel.com>
2023-06-20 00:33:33 +0200

    Add styles to example component

--- src/components/header.component.ts
+++ src/components/header.component.ts
@@ -1,13 +1,21 @@
-import { LitElement, html } from 'lit';
+import { LitElement, html, css } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import { translate } from '../services/i18n.service';
 
 @customElement('header-component')
 export class Header extends LitElement {
+  static styles = css`
+    .header {
+      color: var(--color-red);
+      font-size: 28px;
+      text-align: center;
+    }
+  `;
+
   render() {
     return html`
-      <header>
-        ${translate('firstName')}
+      <header class="header">
+        ${translate('description')}
       </header>
     `;
   }
